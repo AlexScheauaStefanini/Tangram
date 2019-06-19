daf4b8be2eabfb2b64a8ee7166c167d56f6ab51a
Alex Scheaua Stefanini <51158318+AlexScheauaStefanini@users.noreply.github.com>
2019-06-19 13:21:00 +0300

    Revert "Version 1.26.3"
    This reverts commit d5a086407a877584ee0acb805e2cddcdb6f9d254.

--- static/resources/animations/new-level-animations.js
+++ static/resources/animations/new-level-animations.js
@@ -1,10 +1,9 @@
 function newLevelAnimations() {
+
   let gameContainer = document.querySelector("#game");
   let stroke = document.querySelector("#stroke");
   let pieces = document.querySelectorAll(".piece");
   let timer = document.querySelector(".timer-container");
-
-  Origins.originsRecalculation();
 
   gameContainer.classList.add('game-appearing');
   gameContainer.style.opacity = 1;
@@ -18,9 +17,10 @@ function newLevelAnimations() {
   for (let i = 0; i < pieces.length; i++) {
     //asez piesele in forma initiala
     pieces[i].style.top = (Origins.pieceOriginalCoords[i].top + dragOffsetY) + "px";
-    pieces[i].style.left = Origins.pieceOriginalCoords[i].left + "px";
-    pieces[i].style.transform = Origins.pieceOriginalCoords[i].transform;
+    pieces[i].style.left = (Origins.pieceOriginalCoords[i].left + stroke.offsetLeft + 100) + "px";
 
+
+    pieces[i].style.transform = Origins.pieceOriginalCoords[i].transform;
     pieces[i].firstElementChild.classList.remove("in-place");
 
     if (pieces[i].style.transform === "rotate(90deg)") {
@@ -31,8 +31,9 @@ function newLevelAnimations() {
     pieces[i].style.opacity = 1;
   }
 
-  document.querySelector("#game").addEventListener('animationend', () => {
 
+  document.querySelector("#game").addEventListener('animationend', () => {
+    resetOffsets();
     document.querySelector("#game").classList.remove('game-appearing');
     document.querySelector("#stroke").classList.remove('stroke-apearing');
     for (let i = 0; i < pieces.length; i++) {
@@ -43,6 +44,6 @@ function newLevelAnimations() {
       }
     }
     timer.classList.remove('timer-appearing');
-
+    resetPieces();
   }, { once: true })
 }
